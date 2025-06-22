5e4dd29f5a7027c9c2f53ee7b3335a314542efbd
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-22 11:01:09 +0530

    About Page Created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Home from './components/Home'
 
 function App() {
   
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
      <h1>KalaKruti</h1>
-
+<Home/>
     </>
   )
 }
